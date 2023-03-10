09f123d7059f7dc65a4a354c8cf778f5031555e6
sumit1092 <87828844+sumit1092@users.noreply.github.com>
2023-03-10 20:12:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
+import Card from './Card';
+import Title from './Title';
+import Name from './Name';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+      <h1>Task: Add three Card elements</h1>
+      <Title para = "React Props"/>
+      <Card h2="First card is h2" h3="First card is h3"/>
+      <Card h2="Second card is h2" h3="Second card is h3"/>
+      <Card h2="Third card is h2" h3="Third card is h3"/>
+      <Name naam ="Bond007"/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Name.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Name(props){
+    return(
+      <div>
+        <h2>So The Name is {props.naam}</h2>
+      </div>
+    );
+  };
+  export default Name;
